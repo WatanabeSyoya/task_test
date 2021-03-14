bee10e90e097e5f4409b9cd3008e275a80505669
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-03-14 20:35:25 +0900

    store

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -43,8 +43,22 @@ class ContactFormController extends Controller
      */
     public function store(Request $request)
     {
-         $your_name = $request->input('your_name');
-         dd($your_name);
+
+        $contact = new ContactForm;
+
+         $contact->your_name = $request->input('your_name');
+         $contact->title = $request->input('title');
+         $contact->email = $request->input('email');
+         $contact->url = $request->input('url');
+         $contact->gender = $request->input('gender');
+         $contact->age = $request->input('age');
+         $contact->contact = $request->input('contact');
+         
+         $contact->save();
+
+         return redirect('contact/index');
+         
+         
     }
 
     /**
