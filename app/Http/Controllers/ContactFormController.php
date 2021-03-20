613c21fc1b121b5c32d205bfcb67f4bea65d44ed
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-03-20 22:08:52 +0900

    サービスへの切り離し

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Models\ContactForm;
 use Illuminate\Support\Facades\DB;
+use App\Services\CheckFormData;
 
 class ContactFormController extends Controller
 {
@@ -77,33 +78,12 @@ class ContactFormController extends Controller
     {
         $contact = ContactForm::find($id);
         
-        if($contact->gender === 0){
-           $gender = '男性';
-        }
-        if($contact->gender === 1){
-            $gender = '女性';
-         }
+        $gender = CheckFormData::checkGender($contact);
 
-         if($contact->age === 1){
-            $age = '〜19歳';
-         }
-         if($contact->age === 2){
-            $age = '20歳〜29歳';
-         }
-         if($contact->age === 3){
-            $age = '30歳〜39歳';
-         }
-         if($contact->age === 4){
-            $age = '40歳〜49歳';
-         }
-         if($contact->age === 5){
-            $age = '50歳〜59歳';
-         }
-         if($contact->age === 6){
-            $age = '60歳〜';
-         }
+        $age = CheckFormData::checkAge($contact);
 
-        return view('contact.show', compact('contact', 'gender' , 'age'));
+        return view('contact.show', 
+        compact('contact','gender','age'));
     }
 
     /**
@@ -128,7 +108,19 @@ class ContactFormController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $contact = ContactForm::find($id);
+
+        $contact->your_name = $request->input('your_name');
+        $contact->title = $request->input('title');
+        $contact->email = $request->input('email');
+        $contact->url = $request->input('url');
+        $contact->gender = $request->input('gender');
+        $contact->age = $request->input('age');
+        $contact->contact = $request->input('contact');
+        
+        $contact->save();
+
+        return redirect('contact/index');
     }
 
     /**
@@ -139,6 +131,9 @@ class ContactFormController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $contact = ContactForm::find($id);
+        $contact->delete();
+
+        return redirect('contact/index');
     }
 }
--- resources/views/contact/edit.blade.php
+++ resources/views/contact/edit.blade.php
@@ -25,7 +25,7 @@
                     @endif
                     
                     editです
-                    <form method="POST" action="">
+                    <form method="POST" action="{{ route('contact.update', ['id' => $contact->id ]) }}">
                     @csrf
                     氏名
                     <input type="text" name="your_name" value="{{ $contact->your_name }}">
@@ -46,12 +46,12 @@
                     年齢
                     <select name="age">
                     <option value="">選択してください</option>
-                    <option value="1">〜19歳</option>
-                    <option value="2">20歳〜29歳</option>
-                    <option value="3">30歳〜39歳</option>
-                    <option value="4">40歳〜49歳</option>
-                    <option value="5">50歳〜59歳</option>
-                    <option value="6">60歳〜</option>
+                    <option value="1" @if($contact->age === 1 ) selected @endif >〜19歳</option>
+                    <option value="2" @if($contact->age === 2 ) selected @endif >20歳〜29歳</option>
+                    <option value="3" @if($contact->age === 3 ) selected @endif >30歳〜39歳</option>
+                    <option value="4" @if($contact->age === 4 ) selected @endif >40歳〜49歳</option>
+                    <option value="5" @if($contact->age === 5 ) selected @endif >50歳〜59歳</option>
+                    <option value="6" @if($contact->age === 6 ) selected @endif >60歳〜</option>
                     </select>
                     <br>
                     お問い合わせ内容
