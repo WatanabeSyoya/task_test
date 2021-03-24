6a3f582fc7778f53518bce96e9d6f7cdae48954a
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-03-24 22:31:34 +0900

    home

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,4 @@ Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function(){
 
 Auth::routes();
 
-//Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
